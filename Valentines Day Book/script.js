a5e99224e26057801b77aadf678826045a76c8c2
Alok Patel <alokpatel41001@gmail.com>
2025-02-14 21:27:42 +0530

    third commit

--- Valentines Day Book/script.js
+++ Valentines Day Book/script.js
@@ -13,6 +13,6 @@ form.addEventListener('submit', (e) => {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
-		resultDiv.innerHTML = 'Naa Tum meri baby nhi hoo kon hooo tum kaha hai meri Chotu';
+		resultDiv.innerHTML = 'Naa Tum meri chotu nhi hoo kon hooo tum kaha hai meri Chotu';
 	}
 });
